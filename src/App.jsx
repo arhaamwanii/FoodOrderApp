dee9b33b7c3c416a39bb84e4bce59c1909c81935
arhaamwanii <arhamwani765@gmail.com>
2024-03-26 21:36:28 +0530

    code is barely wroking lol but it is working just don't even move a single line of code you will be good to go ]

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import { Meals } from './components/Meals';
 
 function App() {
   const [orderData , setOrderData] = useState(
-    {
-    order: {
-      items: [
-        { name: undefined, quantity: null },
-        { name: undefined, quantity: null }
-      ],
-      customer: {
-        name: undefined,
-        email: undefined,
-        street: null ,
-        postalcode: null,
-        city: null
-      }
-    }
-  }
+  //   {
+  //   order: {
+  //     items: [
+  //       { name: undefined, quantity: null }
+  //     ],
+  //     customer: {
+  //       name: undefined,
+  //       email: undefined,
+  //       street: null ,
+  //       postalcode: null,
+  //       city: null
+  //     }
+  //   }
+  // }
   )
 
   return (
--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+    let updatedCart = null
 
 export const MealCard = ({image , title , price , description , orderData, setOrderData }) => {
     
     function handleAddToCart(){
-        const updatedCart = {
+
+        if (updatedCart === null){
+         updatedCart = {
             order: {
                 items : [
-                    ...orderData.order.items,
                     {name : title , quantity : 1} 
                 ],
                 customer: {
@@ -18,11 +20,53 @@ export const MealCard = ({image , title , price , description , orderData, setOr
                   }
             }
         }
-        setOrderData(updatedCart)
-        console.log(updatedCart)
+        }else{
+            let obj = updatedCart.order.items.find(item => item.name === title);
+            let inputObject 
+            if(obj){
+                obj.quantity++
+                // inputObject = obj
+                updatedCart.order.items.find(item => item.name === title).quantity++
+                // well we are still entering the new object rather then editting the older one
+                // logic should be pretty simple check if the thing that is being entered already exits if it does instead of entering a new one upate the older oen
+
+            }else{
+                inputObject =  {name : title , quantity : 1}
+            }
+
+             updatedCart = {
+                order: {
+                    items : [
+                        ...orderData.order.items,
+                        inputObject
+                        // {name : title , quantity : 1} ,
+                    ],
+                    customer: {
+                        name: undefined,
+                        email: undefined, 
+                        street: null ,
+                        postalcode: null,
+                        city: null
+                      }
+                }
+            }
+            
+
+             console.log(obj)
+
+             if(obj){
+                obj.quantity++
+                obj = updatedCart.order.items.find(item => item.name === title)
+             }
+        }
+        setOrderData(updatedCart ? updatedCart : null)
+  
+        return updatedCart
+        //so we have an object in there which updates on each click, 
+        // so for every click we need to check if the thing we clicked is already in there if it put
     }
 
-
+      console.log(orderData)
   
     return (
     <div>
@@ -30,7 +74,7 @@ export const MealCard = ({image , title , price , description , orderData, setOr
         <h3>{title}</h3>
         <p>{price}</p>
         <p>{description}</p>
-        <button style={{cursor : "pointer"}} onClick={() => {handleAddToCart()}}>Add to Card</button>
+        <button style={{cursor : "pointer"}} onClick={handleAddToCart}>Add to Card</button>
     </div>
   )
 }
