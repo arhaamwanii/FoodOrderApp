772231c192df6b444298c789ae0205afccca2fe4
arhaamwanii <arhamwani765@gmail.com>
2024-03-26 22:57:15 +0530

    this code is workig for the initla cart adding now --- buch of things put togther and there you have it it is pretty muc working now

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
     let updatedCart = null
 
 export const MealCard = ({image , title , price , description , orderData, setOrderData }) => {
-    
+
     function handleAddToCart(){
 
         if (updatedCart === null){
@@ -21,25 +21,27 @@ export const MealCard = ({image , title , price , description , orderData, setOr
             }
         }
         }else{
+
             let obj = updatedCart.order.items.find(item => item.name === title);
             let inputObject 
             if(obj){
-                obj.quantity++
+                // obj.quantity++
                 // inputObject = obj
                 updatedCart.order.items.find(item => item.name === title).quantity++
-                // well we are still entering the new object rather then editting the older one
-                // logic should be pretty simple check if the thing that is being entered already exits if it does instead of entering a new one upate the older oen
-
+               // so we are updating the thing that already exists 
+            // for some reaoson this code is being executed -- more then once  
+                
             }else{
                 inputObject =  {name : title , quantity : 1}
+                updatedCart.order.items.push( {name : title , quantity : 1} )
             }
-
+    
              updatedCart = {
                 order: {
                     items : [
                         ...orderData.order.items,
-                        inputObject
                         // {name : title , quantity : 1} ,
+                        // inputObject -- this was causing undefined thing that is solved
                     ],
                     customer: {
                         name: undefined,
@@ -50,21 +52,11 @@ export const MealCard = ({image , title , price , description , orderData, setOr
                       }
                 }
             }
-            
-
              console.log(obj)
-
-             if(obj){
-                obj.quantity++
-                obj = updatedCart.order.items.find(item => item.name === title)
-             }
         }
         setOrderData(updatedCart ? updatedCart : null)
-  
         return updatedCart
-        //so we have an object in there which updates on each click, 
-        // so for every click we need to check if the thing we clicked is already in there if it put
-    }
+    }   
 
       console.log(orderData)
   
@@ -80,3 +72,12 @@ export const MealCard = ({image , title , price , description , orderData, setOr
 }
 // 1 till 4:30 sleep
 //  5:30 back to sleep
+//so we have an object in there which updates on each click, 
+        // so for every click we need to check if the thing we clicked is already in there if it put
+        //  we code for 5 horus -- then we study for like 4 hours ok! sounds fair enough so
+        // so have to get in that zone of like coding for 2 session coding 2 session studying that should be it 
+        // 8pm to 4 am --> 8hr stream 50 min break 
+        // 2x coding sessions -- 2x studying sessions 
+        // math 12 chapter we have : --- till saturday coding + maht 
+        // till then sunday and monday we do math whole day
+        // bro for the next some time all am going to be doing is studying then -- next some time alll i 
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -20,7 +20,6 @@ export const Meals = ({orderData , setOrderData}) => {
       }   
       fetchAvailableMeals()
     } , [] )
-    
 
 
     return (
